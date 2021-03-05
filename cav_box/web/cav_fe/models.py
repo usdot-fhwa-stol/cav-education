168787abab9deb59fc0de8b27b07e6b712a01164
itabrl <samir.manafzadehtabriz@leidos.com>
2021-03-04 21:57:45 -0600

    update models

--- cav_box/web/cav_fe/models.py
+++ cav_box/web/cav_fe/models.py
@@ -1,9 +1,8 @@
 from django.db import models
 
-# Create your models here.
-from django.db import models
 class incomming_dsrc_message(models.Model):
-    messageId=models.CharField(max_length=100)
-    value=models.CharField(max_length=150)
-    timestamp=models.CharField(max_length=100)
-    payload=models.CharField(max_length=1000)
+    id = models.AutoField(primary_key=True)
+    timestamp = models.CharField(max_length=100)
+    message_type = models.CharField(max_length=10)
+    payload = models.CharField(max_length=4000)
+    original_message = models.CharField(max_length=4000)

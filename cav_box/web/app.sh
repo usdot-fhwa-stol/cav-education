02ca36af6f28f200b085a3478f2e02889d55b77f
itabrl <samir.manafzadehtabriz@leidos.com>
2021-03-17 10:12:32 -0500

    Update web

--- cav_box/web/app.sh
+++ cav_box/web/app.sh
@@ -1,4 +1,5 @@
-python manage.py inspectdb > cav_fe/models.py
+# python manage.py inspectdb > cav_fe/models.py
+python manage.py makemigrations
 python manage.py migrate
-python manage.py createsuperuser --username admin --password admin
+python manage.py shell -c "from django.contrib.auth.models import User; User.objects.create_superuser('admin', 'cav@cav.com', 'admin')"
 python manage.py runserver 0.0.0.0:8000

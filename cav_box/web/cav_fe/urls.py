8f55437bf08bec28aac235296f2c114758b476b1
dan-du-car <dan.du@leidos.com>
2021-03-17 15:46:01 -0400

    update

--- cav_box/web/cav_fe/urls.py
+++ cav_box/web/cav_fe/urls.py
@@ -1,11 +1,8 @@
 from django.urls import path
-# from django.contrib.staticfiles.urls import staticfiles_urlpatterns
-# urlpatterns += staticfiles_urlpatterns()
 
 from . import views
 
 urlpatterns = [
-    #path('', views.index, name='index'),
     path('', views.index, name='index'),
     path('<int:user_id>/', views.detail, name='detail'),
     path('post/ajax/message_filters', views.postMessageFilters, name = "post_message_filters")

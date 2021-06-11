811aca0a0b65933215d029bb595f026665b3aa3b
itabrl <samir.manafzadehtabriz@leidos.com>
2021-06-11 16:45:15 -0500

    add license

--- cav_box/kafka/src/producers/dsrc.py
+++ cav_box/kafka/src/producers/dsrc.py
@@ -1,4 +1,18 @@
-"""Methods pertaining to loading and configuring CTA "L" station data."""
+#  *
+#  * Copyright (C) 2019-2020 LEIDOS.
+#  *
+#  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+#  * use this file except in compliance with the License. You may obtain a copy of
+#  * the License at
+#  *
+#  * http://www.apache.org/licenses/LICENSE-2.0
+#  * Unless required by applicable law or agreed to in writing, software
+#  * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#  * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#  * License for the specific language governing permissions and limitations under
+#  * the License.
+#  *
+
 import logging
 from pathlib import Path
 from confluent_kafka import avro
@@ -25,7 +39,6 @@ class Dsrc(Producer):
             num_replicas=1,
         )
 
-        # self.message_id = int(message_id)
         self.message_type = message_type
         self.original_message = original_message
         self.payload = payload

d00321bf84cf3be11cc63b1cf41f77ff451b8e21
Ily <49001908+itabrl@users.noreply.github.com>
2021-06-21 09:58:37 -0400

    Feature/flask (#32)
    * update map
    
    * remove faust
    
    * add license

--- cav_box/flask/app.py
+++ cav_box/flask/app.py
@@ -1,5 +1,21 @@
+#  *
+#  * Copyright (C) 2019-2020 LEIDOS.
+#  *
+#  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+#  * use this file except in compliance with the License. You may obtain a copy of
+#  * the License at
+#  *
+#  * http://www.apache.org/licenses/LICENSE-2.0
+#  * Unless required by applicable law or agreed to in writing, software
+#  * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#  * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#  * License for the specific language governing permissions and limitations under
+#  * the License.
+#  *
+
 from flask import Flask, render_template, Response
 from pykafka import KafkaClient
+from pykafka.common import OffsetType
 
 import avro.schema
 from avro.datafile import DataFileReader, DataFileWriter
@@ -40,7 +56,7 @@ def index():
 def get_messages(topicname):
     client = get_kafka_client()
     def events():
-        for i in client.topics[topicname].get_simple_consumer():
+        for i in client.topics[topicname].get_simple_consumer(auto_offset_reset=OffsetType.LATEST, reset_offset_on_start=True):
             data = decode(i.value)
             try:
                 data["payload"] = json.loads(data["payload"])
